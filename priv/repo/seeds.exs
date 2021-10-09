9a942d0653fdaf7c8d1c351f2e14e077f4884797
jakewmiles <jake.w.miles@gmail.com>
2021-10-09 14:39:53 +0100

    feat: update seed

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,12 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+board = Kanban.Repo.insert!(%Kanban.Board{title: "Awesome project"})
+
+backlog = Kanban.Repo.insert!(%Kanban.Column{title: "Backlog", board_id: board.id})
+
+_in_progress = Kanban.Repo.insert!(%Kanban.Column{title: "In progress", board_id: board.id})
+_done = Kanban.Repo.insert!(%Kanban.Column{title: "Done", board_id: board.id})
+
+_card = Kanban.Repo.insert!(%Kanban.Card{content: "Put some nice cat picture on the homepage", column_id: backlog.id})
