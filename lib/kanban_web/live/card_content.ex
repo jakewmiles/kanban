b8585e644e0f7c70ee6b2ba7fbe41c37ec5ef14d
jakewmiles <jake.w.miles@gmail.com>
2021-10-09 17:57:48 +0100

    feat: implement delete functionality

--- lib/kanban_web/live/card_content.ex
+++ lib/kanban_web/live/card_content.ex
@@ -2,11 +2,14 @@ defmodule KanbanWeb.CardContent do
   use Surface.Component
   prop id, :string, required: true
   prop content, :string, required: true
+  prop card, :struct, required: true
+  require Logger
 
   def render(assigns) do
     ~F"""
-    <div data-card-id={@id} class="task alert alert-success" role="alert">
+    <div data-card-id={@id} class="task alert alert-success">
       <textarea phx-blur="update_card" phx-value-card={@id}>{@content}</textarea>
+      <h3 phx-value-to_delete={@id} phx-click="delete_card">⨯</h3>
     </div>
     """
   end
--- lib/kanban_web/live/page_live.ex
+++ lib/kanban_web/live/page_live.ex
@@ -1,10 +1,12 @@
 defmodule KanbanWeb.PageLive do
   use Surface.LiveView
   alias KanbanWeb.ColumnContent
+  alias Kanban.Card
+  alias Kanban.Board
   require Logger
 
   def mount(_params, %{"board_id" => board_id}, socket) do
-    with {:ok, board} <- Kanban.Board.find(board_id) do
+    with {:ok, board} <- Board.find(board_id) do
       KanbanWeb.Endpoint.subscribe(topic(board_id))
       {:ok,
         socket
@@ -28,9 +30,9 @@ defmodule KanbanWeb.PageLive do
 
   def handle_event("add_card", %{"column" => column_id}, socket) do
     {id, _} = Integer.parse(column_id)
-    new_card = %Kanban.Card{column_id: id, content: "Something new"}
+    new_card = %Card{column_id: id, content: "Something new"}
     Kanban.Repo.insert!(new_card)
-    {:ok, new_board} = Kanban.Board.find(socket.assigns.board.id)
+    {:ok, new_board} = Board.find(socket.assigns.board.id)
     KanbanWeb.Endpoint.broadcast(topic(new_board.id), "new_board", new_board)
     {:noreply, 
       socket
@@ -40,13 +42,26 @@ defmodule KanbanWeb.PageLive do
 
   def handle_event("update_card", %{"card" => card_id, "value" => new_content}, socket) do
     {id, _} = Integer.parse(card_id)
-    Kanban.Card.update(id, %{content: new_content})
-    {:ok, new_board} = Kanban.Board.find(socket.assigns.board.id)
+    Card.update(id, %{content: new_content})
+    {:ok, new_board} = Board.find(socket.assigns.board.id)
     KanbanWeb.Endpoint.broadcast(topic(new_board.id), "new_board", new_board)
     {:noreply,
       socket
       |> assign(board: new_board)
     }
+  end
+
+  def handle_event("delete_card", %{"to_delete" => id}, socket) do
+    Logger.info(id)
+    to_delete = Kanban.Repo.get(Card, id)
+    Kanban.Repo.delete!(to_delete)
+    {:ok, new_board} = Board.find(socket.assigns.board.id)
+    KanbanWeb.Endpoint.broadcast(topic(new_board.id), "new_board", new_board)
+    {:noreply,
+      socket
+      |> assign(board: new_board)
+    }
+    {:noreply, socket}
   end
 
   def handle_info(%{topic: message_topic, event: "new_board", payload: new_board}, socket) do
